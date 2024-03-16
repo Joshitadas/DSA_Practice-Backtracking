bc781d32f4c6c7e2c5274a2cfb84e4ad0d7fee0f
Anika Kamath <87904385+anika-kamath@users.noreply.github.com>
2024-03-16 11:02:22 +0530

    Create NextPermutation.cpp

---
+++ Miscellaneous/NextPermutation.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int idx = -1;
+        int n = nums.size();
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                idx = i;
+                break;
+            }
+        }
+        if (idx != -1) {
+            for (int i = n - 1; i >= idx + 1; i--) {
+                if (nums[i] > nums[idx]) {
+                    swap(nums[i], nums[idx]);
+                    break;
+                }
+            }
+        }
+
+        int start = idx + 1, end = n - 1;
+        while (start < end) {
+            swap(nums[start], nums[end]);
+            start++;
+            end--;
+        }
+    }
+};
